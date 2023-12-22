07d86f7e49084e9625896faecd4bcfa2473e3014
David Feldt <dsfeldt@gmail.com>
2023-12-22 11:58:20 -0500

    Successfully load site

--- Main Bot.py
+++ Main Bot.py
@@ -79,59 +79,97 @@ from random_user_agent.params import SoftwareName, OperatingSystem
 # OLD PROXY CODE  ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # TO DO: Get user agent working, figure out whats crashing chrome
 
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome()
 
-driver.get("https://sslproxies.org/")
-driver.execute_script("return arguments[0].scrollIntoView(true);", WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//table[@class='table table-striped table-bordered']//th[contains(., 'IP Address')]"))))
-ips = [my_elem.get_attribute("innerHTML") for my_elem in WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//table[@class='table table-striped table-bordered']//tbody//tr/td[1]")))]
-ports = [my_elem.get_attribute("innerHTML") for my_elem in WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//table[@class='table table-striped table-bordered']//tbody//tr/td[2]")))]
-driver.quit()
+# driver.get("https://sslproxies.org/")
+# driver.execute_script("return arguments[0].scrollIntoView(true);", WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//table[@class='table table-striped table-bordered']//th[contains(., 'IP Address')]"))))
+# ips = [my_elem.get_attribute("innerHTML") for my_elem in WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//table[@class='table table-striped table-bordered']//tbody//tr/td[1]")))]
+# ports = [my_elem.get_attribute("innerHTML") for my_elem in WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//table[@class='table table-striped table-bordered']//tbody//tr/td[2]")))]
+# driver.quit()
 
-proxies = []
-for i in range(0, len(ips)):
-    proxies.append(ips[i]+':'+ports[i])
-print(proxies)
-for i in range(0, len(proxies)):
-    try:
-        print("Proxy selected: {}".format(proxies[i]))
-        options = webdriver.ChromeOptions()
-        #New Code 
-        software_names = [SoftwareName.CHROME.value]
-        operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]
-        print("Init software_names", software_names)
-        user_agent_rotator = UserAgent(software_names=software_names, operating_system=operating_systems,limit=100)
-        print("Init user_agent_rotator", user_agent_rotator)
-        user_agent =user_agent_rotator.get_random_user_agent()
-        print("Init user_agent", user_agent)    
-        #options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--disable-gpu')
-        options.add_argument(f'user-agent={user_agent}')
-        # Old stuff
-        #ptions.add_argument('--proxy-server=%s' % proxies[i])
-        driver = webdriver.Chrome(options)
-        driver.get('https://www.whatismyip.com')
-        print("My IP", proxies[i])
-        time.sleep(5000)
-        time_to_wait = 90
-        try:
-            WebDriverWait(driver, time_to_wait).until(EC.presence_of_element_located((By.CLASS_NAME, class_name)))
-        finally:
-            driver.maximize_window()
-            page_html = driver.page_source
-            driver.close()
-            print(page_html)
+# proxies = []
+# for i in range(0, len(ips)):
+#     proxies.append(ips[i]+':'+ports[i])
+# print(proxies)
+# for i in range(0, len(proxies)):
+#     try:
+#         print("Proxy selected: {}".format(proxies[i]))
+#         options = webdriver.ChromeOptions()
+#         #New Code 
+#         software_names = [SoftwareName.CHROME.value]
+#         operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]
+#         print("Init software_names", software_names)
+#         user_agent_rotator = UserAgent(software_names=software_names, operating_system=operating_systems,limit=100)
+#         print("Init user_agent_rotator", user_agent_rotator)
+#         user_agent =user_agent_rotator.get_random_user_agent()
+#         print("Init user_agent", user_agent)    
+#         #options.add_argument('--headless')
+#         options.add_argument('--no-sandbox')
+#         options.add_argument('--disable-dev-shm-usage')
+#         options.add_argument('--disable-gpu')
+#         options.add_argument(f'user-agent={user_agent}')
+#         # Old stuff
+#         #ptions.add_argument('--proxy-server=%s' % proxies[i])
+#         driver = webdriver.Chrome(options)
+#         driver.get('https://www.whatismyip.com')
+#         print("My IP", proxies[i])
+#         time.sleep(5000)
+#         time_to_wait = 90
+#         try:
+#             WebDriverWait(driver, time_to_wait).until(EC.presence_of_element_located((By.CLASS_NAME, class_name)))
+#         finally:
+#             driver.maximize_window()
+#             page_html = driver.page_source
+#             driver.close()
+#             print(page_html)
         
 
-        # driver.get("https://www.whatismyip.com/proxy-check/?iref=home")
-        # if "Proxy Type" in WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "p.card-text"))):
-        #     break
-    except Exception as e:
-        print("Exception: ", str(e))
-        driver.quit()
+#         # driver.get("https://www.whatismyip.com/proxy-check/?iref=home")
+#         # if "Proxy Type" in WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "p.card-text"))):
+#         #     break
+#     except Exception as e:
+#         print("Exception: ", str(e))
+#         driver.quit()
 # OLD PROXY CODE  ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+# NO PROXY CODE  ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+try:
+    options = webdriver.ChromeOptions()
+    #New Code 
+    software_names = [SoftwareName.CHROME.value]
+    operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]
+    print("Init software_names", software_names)
+    user_agent_rotator = UserAgent(software_names=software_names, operating_system=operating_systems,limit=100)
+    print("Init user_agent_rotator", user_agent_rotator)
+    user_agent =user_agent_rotator.get_random_user_agent()
+    print("Init user_agent", user_agent)    
+    #options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument(f'user-agent={user_agent}')
+    # Old stuff
+    driver = webdriver.Chrome(options)
+    driver.get('https://www.facebook.com')
+    input()
+    #time.sleep(5000)
+    driver.get('https://m.facebook.com/marketplace/')
+    time_to_wait = 90
+    try:
+        WebDriverWait(driver, time_to_wait).until(EC.presence_of_element_located((By.CLASS_NAME, 'html-renderer')))
+    finally:
+        input()
+        driver.maximize_window()
+        page_html = driver.page_source
+        driver.close()
+        with open('output.txt', 'w') as file:
+            file.write(page_html)
+            print("Page HTML saved to output.txt")
+except Exception as e:
+        print("Exception: ", str(e))
+        driver.quit()
+
+# NO PROXY CODE  ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # SIMPLE CODE ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # try:
 #     # Open the website
